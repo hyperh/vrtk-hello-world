c47f23db19d97be9862f018dfad1b7544b5e6cd7
HL <hyperh@users.noreply.github.com>
2019-01-16 23:10:45 -0500

    Fracture monkey on collision and play sound

---
+++ Assets/MyStuff/Scripts/Fracturable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fracturable : MonoBehaviour {
+
+	public GameObject fractured;
+	public AudioSource fractureSound;
+
+	public void Fracture() {
+		Instantiate(fractured, transform.position, transform.rotation);
+		Destroy(gameObject);
+		fractureSound.Play();
+	}
+
+	void OnMouseDown() {
+		Fracture();
+	}
+}
---
+++ Assets/MyStuff/Scripts/FractureOnCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Fracturable))]
+public class FractureOnCollision : MonoBehaviour {
+
+	public int fractureVelocity;
+	Fracturable fracturable;
+
+	void Start() {
+		fracturable = GetComponent<Fracturable>();
+	}
+	void OnCollisionEnter(Collision col) {
+		Debug.LogFormat("FractureOnCollision.OnCollisionEnter: {0}", col.relativeVelocity.magnitude);
+		if (col.relativeVelocity.magnitude >= fractureVelocity) {
+			fracturable.Fracture();
+		}
+	}
+}
